619a439b052944d7e41378721d4f44d27115708d
Xeroth95 <shillah@hotmail.de>
2014-02-07 15:16:06 +0100

    Adding initial Data

---
+++ src/main.c
@@ -0,0 +1,31 @@
+/* -*- Mode: C; indent-tabs-mode: t; c-basic-offset: 8; tab-width: 8 -*-  */
+/*
+ * main.c
+ * Copyright (C) 2014 Xeroth95 <shillah@hotmail.de>
+ * 
+ * MyFileSystem is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * MyFileSystem is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+int create_fs( char *name );
+
+int main( int argc, char **argv )
+{
+	return 0;
+}
+
+int create_fs( char *name )
+{
+	//create the fs with the name name
+	return 0;
+}
